3c763252af0fef171eb1bc3a7e53aad40c480aee
Marcus0224 <marcstlouis04@gmail.com>
2022-02-20 14:05:05 -0500

    map, filter and join code

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
+ const fs = require('fs');
 
-// const generatePage = require('./src/page-template');
+ const generatePage = require('./src/page-template');
 
 // const pageHTML = generatePage(name, github);
 
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
+//  fs.writeFile('./index.html', pageHTML, err => {
+//    if (err) throw err;
 
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+//    console.log('Portfolio complete! Check out index.html to see the output!');
+//  });
 
   const promptUser = () => {
     return inquirer.prompt([
@@ -139,5 +139,11 @@ const inquirer = require('inquirer');
   promptUser()
     .then(promptProject)
     .then(portfolioData => {
-      console.log(portfolioData);
+       const pageHTML = generatePage(portfolioData);
+
+      fs.writeFile('./index.html', pageHTML, err => {
+         if (err) throw err;
+
+         console.log('Portfolio complete! Check out index.html to see the output!');
+       });
     });
